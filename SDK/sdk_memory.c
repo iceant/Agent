ee9c3bd9ded20000db562ec6c29f5b23d9da1ac9
Chen Peng <pizer.chen@gmail.com>
2024-05-08 22:49:45 +0800

    SDK HashTable Demo

--- SDK/sdk_memory.c
+++ SDK/sdk_memory.c
@@ -16,6 +16,7 @@ static void* sdk_memory__pool = 0;
 void sdk_memory_init(void){
     if(sdk_memory__pool){
         free(sdk_memory__pool);
+        sdk_memory__pool = 0;
     }
     sdk_memory__pool = malloc(TLSF_POOL_SIZE);
     init_memory_pool(TLSF_POOL_SIZE, sdk_memory__pool);
